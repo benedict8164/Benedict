182151682204c43b9c2e5321621a3949509a12ee
Benedict solano <119546263+benedict8164@users.noreply.github.com>
2023-05-12 08:09:25 +0800

    about done

--- index.js
+++ index.js
@@ -25,14 +25,15 @@ const navSlide = function (){
 
 navSlide();
 
-// This code is for scroll smooth effect
+// This code is for scroll smooth effect left to right
 
 const observer = new IntersectionObserver((entries)=>{
     entries.forEach((entry)=>{
         console.log(entry)
         if(entry.isIntersecting){
             entry.target.classList.add("show");
-        }else{
+        }
+        else{
             entry.target.classList.remove("show")
         }
     });
@@ -41,4 +42,22 @@ const observer = new IntersectionObserver((entries)=>{
 
 const hiddenElements =document.querySelectorAll(".hidden");
 hiddenElements.forEach((el)=>observer.observe(el));
+
+// This code is for scroll smooth effect about  
+
+const observer1 = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add("show1");
+        }
+        else{
+            entry.target.classList.remove("show1")
+        }
+    });
+
+});
+
+const hiddenElements1 =document.querySelectorAll(".hidden1");
+hiddenElements1.forEach((el)=>observer1.observe(el));
 

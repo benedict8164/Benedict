e78d27f72fe87ae46b9792f8453751923f7e0e07
Benedict solano <119546263+benedict8164@users.noreply.github.com>
2023-05-17 12:09:07 +0800

    tertimonials done

--- index.js
+++ index.js
@@ -61,3 +61,21 @@ const observer1 = new IntersectionObserver((entries)=>{
 const hiddenElements1 =document.querySelectorAll(".hidden1");
 hiddenElements1.forEach((el)=>observer1.observe(el));
 
+// this code is right to left animation
+
+const observer2 = new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add("show2");
+        }
+        else{
+            entry.target.classList.remove("show2")
+        }
+    });
+
+});
+
+const hiddenElements2 =document.querySelectorAll(".hidden2");
+hiddenElements2.forEach((el)=>observer2.observe(el));
+

6435ad8be3c357d12f43c92bd49e77b65fb04f2f
Benedict solano <119546263+benedict8164@users.noreply.github.com>
2023-05-22 16:09:02 +0800

    functional done

--- index.js
+++ index.js
@@ -79,3 +79,104 @@ const observer2 = new IntersectionObserver((entries)=>{
 const hiddenElements2 =document.querySelectorAll(".hidden2");
 hiddenElements2.forEach((el)=>observer2.observe(el));
 
+// smooth scroll effect home
+
+const navLinks = document.querySelectorAll('a[href^="#home"]');
+const navLinks1 = document.querySelectorAll('a[href^="#what-i-do"]');
+const navLinks2 = document.querySelectorAll('a[href^="#about-me"]');
+const navLinks3 = document.querySelectorAll('a[href^="#testimonials"]');
+const navLinks4 = document.querySelectorAll('a[href^="#skills"]');
+
+
+// Add a click event listener to each navigation link
+navLinks.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+
+    // Get the target section based on the href attribute
+    const target = document.querySelector(link.getAttribute('href'));
+
+    // Scroll to the target section smoothly
+    target.scrollIntoView({
+      behavior: 'smooth'
+    });
+  });
+});
+
+// smooth scroll effect what i do
+
+
+// Add a click event listener to each navigation link
+navLinks1.forEach(link => {
+  link.addEventListener('click', e => {
+    e.preventDefault();
+
+    // Get the target section based on the href attribute
+    const target = document.querySelector(link.getAttribute('href'));
+
+    // Scroll to the target section smoothly
+    target.scrollIntoView({
+        behavior: 'smooth'
+    });
+  });
+});
+
+// smooth scroll effect about me
+
+navLinks2.forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+  
+      // Get the target section based on the href attribute
+      const target = document.querySelector(link.getAttribute('href'));
+  
+      // Scroll to the target section smoothly
+      target.scrollIntoView({
+          behavior: 'smooth'
+      });
+    });
+  });
+
+  // smooth scroll effect testimonials
+
+navLinks3.forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+  
+      // Get the target section based on the href attribute
+      const target = document.querySelector(link.getAttribute('href'));
+  
+      // Scroll to the target section smoothly
+      target.scrollIntoView({
+          behavior: 'smooth'
+      });
+    });
+  });
+
+    // smooth scroll effect myskills
+
+navLinks4.forEach(link => {
+    link.addEventListener('click', e => {
+      e.preventDefault();
+  
+      // Get the target section based on the href attribute
+      const target = document.querySelector(link.getAttribute('href'));
+  
+      // Scroll to the target section smoothly
+      target.scrollIntoView({
+          behavior: 'smooth'
+      });
+    });
+  });
+
+//   cv function
+// Function to open the CV
+function viewCV() {
+    // Replace 'cv.pdf' with the actual path or URL of your CV file
+    const cvUrl = './images/cv.pdf';
+    window.open(cvUrl, '_blank');
+  }
+  
+  // Add a click event listener to the button
+  const cvButton = document.getElementById('cvButton');
+  cvButton.addEventListener('click', viewCV);
